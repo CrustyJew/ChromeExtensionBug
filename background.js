894a2768ca4e5e20ba73edc79abc0aa26341ee30
CrustyJew <mattiprice520@gmail.com>
2017-03-15 12:41:47 -0500

    Working example

--- background.js
+++ background.js
@@ -1,18 +1,14 @@
+window.onload = function(){
+    var frame = document.createElement('iframe');
+    frame.src = 'https://crustyjew.github.io/ChromeExtensionBug/';
+    document.body.appendChild(frame);
 
-window.setTimeout(function(){
-    var ifrmSec = document.createElement("iframe");
-    ifrmSec.src = "https://github.com/";
-    document.body.appendChild(ifrmSec);
-},1000)
-
-window.setTimeout(function(){
-    var ifrmNonSec = document.createElement("iframe");
-    ifrmNonSec.src = "http://www.json.org/";
-    document.body.appendChild(ifrmNonSec);
-},5000)
-
-window.setTimeout(function(){
-    var ifrmSecNoSrc = document.createElement("iframe");
-    document.body.appendChild(ifrmSecNoSrc);
-    ifrmSecNoSrc.src = "https://www.w3schools.com";
-},10000)
+    window.addEventListener('message',function(e){
+    console.log("message received: " + JSON.stringify(e.data));
+});
+    console.log('posting message to iframe');
+    
+    frame.addEventListener('load',function(){
+        frame.contentWindow.postMessage("TestMessage","*");
+    });
+};
