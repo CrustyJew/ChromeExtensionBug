8a20f5b22823361e0142bc4c226fffb0d086d7bb
CrustyJew <CrustyJew@users.noreply.github.com>
2017-03-15 10:44:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload=function(){
   function receiveMessage(e){
+    console.log('received message from source: ' + e.source);
     var origin = event.origin || event.originalEvent.origin; 
     e.source.postMessage({'origin':origin,'message':e.data},"*");
   }
